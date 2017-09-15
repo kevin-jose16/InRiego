a8787cfc35ae9b37c1d8ecb9732ac00578c03f8d
nadia.cabrera <nadia.cabrera@fing.edu.uy>
2017-09-15 19:05:14 -0300

    fragment pivot

--- app/src/main/java/com/example/olave/inriego/MainActivity.java
+++ app/src/main/java/com/example/olave/inriego/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 import layout.Fm_AgregarRiego;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
